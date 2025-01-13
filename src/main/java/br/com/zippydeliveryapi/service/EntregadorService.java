ebfbd3822a02b3faa1f0f27ffc572da2447aacae
Jamilly Anunciada <jamillymilly10@hotmail.com>
2025-01-12 21:28:45 -0300

    create/update/delete entregador

--- src/main/java/br/com/zippydeliveryapi/service/EntregadorService.java
+++ src/main/java/br/com/zippydeliveryapi/service/EntregadorService.java
@@ -2,6 +2,7 @@ package br.com.zippydeliveryapi.service;
 
 import br.com.zippydeliveryapi.model.Entregador;
 import br.com.zippydeliveryapi.model.Usuario;
+import br.com.zippydeliveryapi.model.dto.request.EntregadorRequest;
 import br.com.zippydeliveryapi.repository.EntregadorRepository;
 import br.com.zippydeliveryapi.util.enums.StatusEnum;
 import br.com.zippydeliveryapi.util.exception.EntidadeNaoEncontradaException;
@@ -9,6 +10,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,24 +49,32 @@ public class EntregadorService {
     }
 
     @Transactional
-    public Entregador save(Entregador entregador) {
-        this.usuarioService.save(entregador.getUsuario());
+    public Entregador save(EntregadorRequest request) {
+        Usuario usuario = this.saveUser(request);
+        Entregador entregador = Entregador.fromRequest(request);
         entregador.setHabilitado(Boolean.TRUE);
         entregador.setSenha(this.passwordEncoder.encode(entregador.getSenha()));
-        entregador.setStatus(StatusEnum.PENDENTE.getCodigo());
+        entregador.setUsuario(usuario);
         return this.repository.save(entregador);
     }
 
     @Transactional
-    public void update(Long id, Entregador entregadorAlterado) {
+    public void update(Long id, EntregadorRequest request) {
         Entregador entregador = this.findById(id);
-        entregador.setNome(entregadorAlterado.getNome());
-        entregador.setEmail(entregadorAlterado.getEmail());
-        entregador.setSenha(entregadorAlterado.getSenha());
-        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
-        entregador.setVeiculo(entregadorAlterado.getVeiculo());
-        entregador.setPlaca(entregadorAlterado.getPlaca());
-        entregador.setTelefone(entregadorAlterado.getTelefone());
+
+        if(StringUtils.hasText(request.getEmail()) && !request.getEmail().equals(entregador.getEmail())){
+            entregador.setEmail(request.getEmail());
+            entregador.getUsuario().setUsername(request.getEmail());
+        }
+        if(StringUtils.hasText(request.getSenha()) && !request.getSenha().equals(entregador.getSenha())){
+            entregador.setSenha(request.getSenha());
+            entregador.getUsuario().setPassword(request.getSenha());
+        }
+        entregador.setNome(request.getNome());
+        entregador.setDataNascimento(request.getDataNascimento());
+        entregador.setVeiculo(request.getVeiculo());
+        entregador.setPlaca(request.getPlaca());
+        entregador.setTelefone(request.getTelefone());
         this.repository.save(entregador);
     }
 
@@ -76,13 +87,35 @@ public class EntregadorService {
 
     @Transactional
     public void delete(Long id) {
-        Entregador entregador = this.findById(id);
-        entregador.setHabilitado(Boolean.FALSE);
-        entregador.setCpf(String.format("deleted-%d-%s", entregador.getId(), entregador.getCpf()));
-        entregador.setEmail(String.format("deleted-%d-%s", entregador.getId(), entregador.getEmail()));
-        entregador.getUsuario().setUsername(String.format("deleted-%d-%s", entregador.getUsuario().getId(), entregador.getUsuario().getUsername()));
-        entregador.getUsuario().setPassword(String.format("deleted-%d-%s", entregador.getUsuario().getId(), entregador.getUsuario().getPassword()));
-        entregador.getUsuario().setHabilitado(Boolean.FALSE);
-        this.repository.save(entregador);
+        Entregador entregador = this.repository.findByIdAndHabilitadoTrue(id);
+        if(entregador != null) {
+            entregador.setHabilitado(Boolean.FALSE);
+            entregador.setCpf(String.format("deleted-%d-%s", entregador.getId(), entregador.getCpf()));
+            entregador.setEmail(String.format("deleted-%d-%s", entregador.getId(), entregador.getEmail()));
+            entregador.getUsuario().setUsername(String.format("deleted-%d-%s", entregador.getUsuario().getId(), entregador.getUsuario().getUsername()));
+            entregador.getUsuario().setPassword(String.format("deleted-%d-%s", entregador.getUsuario().getId(), entregador.getUsuario().getPassword()));
+            entregador.getUsuario().setHabilitado(Boolean.FALSE);
+            this.repository.save(entregador);
+        }
+    }
+
+    @Transactional
+    private Usuario saveUser(EntregadorRequest request) {
+        Usuario usuario = null;
+        try {
+            usuario = this.usuarioService.findByUsername(request.getEmail());
+            if(usuario == null){
+                usuario = Usuario.builder()
+                        .roles(Arrays.asList(Usuario.ROLE_ENTREGADOR))
+                        .username(request.getEmail())
+                        .password(request.getSenha())
+                        .build();
+                return this.usuarioService.save(usuario);
+            } else {
+                throw new IllegalArgumentException("Já existe um usuário com o e-mail: " + request.getEmail());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao salvar o usuário: " + e.getMessage(), e);
+        }
     }
 }
